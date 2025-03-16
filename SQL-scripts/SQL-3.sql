2fc4505c938adacb83b90c1d0c1f39f86d17e43d
rahulkumar2022 <rahul.kumarcs22@gmail.com>
2025-03-16 16:28:08 +0530

    sql Queryies using group by and Having

--- SQL-scripts/SQL-3.sql
+++ SQL-scripts/SQL-3.sql
@@ -7,3 +7,11 @@ employees GROUP BY department;
 ---- get the department and Sum(salary) in ascending order
 SELECT department, SUM(salary) AS total_salary FROM employees 
 GROUP BY department ORDER BY SUM(salary) ASC;
+
+--- Get department wise average salary from each departments
+SELECT department, AVG(salary) as avg_salary from employees
+GROUP BY department ORDER BY AVG(salary) DESC;
+
+--- Write down the query to fetch Project name assign to more than one Employee
+SELRCT project_name, COUNT(*) as no_of_employees FROM employees
+GROUP BY project_name HAVING COUNT(*) > 2;
